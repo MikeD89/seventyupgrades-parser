581eb52750dbe7859983c57b02525b12be1d3103
Mike D <michaelduller@hotmail.com>
2021-02-27 02:40:07 +0000

    simplify

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// serve files from the public directory
+app.use(express.static('public'));
+app.use(express.static('src'));
+
+// start the express web server listening on 8080
+app.listen(8080, () => {
+  console.log('listening on 8080');
+});
+
+// serve the homepage
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/src/index.html');
+});
---
+++ src/actions.js
@@ -0,0 +1,5 @@
+// Button clicked
+$("#submitButton").click(function () {
+    const url = $("#inputBox").val()
+    $("#outputBox").val(url);
+});
