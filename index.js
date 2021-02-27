e5acf792a63bbcfdd151fb5ceff6a1f4c9e3904f
Mike D <michaelduller@hotmail.com>
2021-02-27 02:44:05 +0000

    ahh i see

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 // serve files from the public directory
 app.use(express.static('public'));
 app.use(express.static('src'));
 
 // start the express web server listening on 8080
-app.listen(80, () => {
-  console.log('listening on 80');
+app.listen(PORT, () => {
+  console.log('listening on ' + PORT);
 });
 
 // serve the homepage
